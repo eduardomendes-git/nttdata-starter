98fd6709716aa79619d2b6aaef1d4ca142253507
Eduardo Mendes <armenio.eduardo.mendes@gmail.com>
2022-03-26 23:24:35 +0000

    Add JWT classes/config brought over from javainuse.com. Performed some modifications for simplicity

---
+++ src/main/java/com/edu/publiclibrary/jwt/model/JwtResponse.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.edu.publiclibrary.jwt.model;
+
+import java.io.Serializable;
+
+/**
+ * @author	eduardomendes
+ * @date	26 Mar 2022
+ *
+ */
+public class JwtResponse implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private final String jwttoken;
+
+	public JwtResponse(String jwttoken) {
+		this.jwttoken = jwttoken;
+	}
+
+	public String getToken() {
+		return this.jwttoken;
+	}
+}
